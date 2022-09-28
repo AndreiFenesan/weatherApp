02110a736cc5387178422440488a104583aeb546
Andrei <Andrei>
2022-09-28 19:30:42 +0300

    Added feature: Ability display multiple locations on the scrren

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,21 +3,22 @@ import './App.css';
 import LocationInput from "../LocationInput/LocationInput";
 import WeatherCard from "../weather card/WeatherCard";
 import FetchError from "../error component/FetchError";
+interface LocationData{
+    locationName:string,
+    timezone:number,
+    description:string,
+    icon:string,
+    temperature:number,
+    humidity:number,
+    pressure:number,
+    windSpeed:number,
+}
+
 function App() {
     const [lastIntroducedLocationName,setLastIntroducedLocationName] = React.useState("");
-    const [locationData,setLocationData] = React.useState(
-        {
-            locationName:"",
-            timezone:0,
-            description:"",
-            icon:"",
-            temperature:0,
-            humidity:0,
-            pressure:0,
-            windSpeed:0,
-            });
+    const [locationsData,setLocationsData] = React.useState<LocationData[]>([]);
 
-    const [fetchingError,setFetchingError] = React.useState<Error | null>(null);
+    const [fetchError,setFetchError] = React.useState<Error | null>(null);
 
     function searchButtonClickHandler(locationName:string):void{
         //sets the introduced value for lastIntroducedLocationName
@@ -27,7 +28,7 @@ function App() {
     }
 
     React.useEffect(()=>{
-        function extractWeatherData(data:any){
+        function extractWeatherData(data:any):LocationData{
             const locationName:string = data.name;
             const timezone:number = data.timezone;
             const description:string = data.weather[0].description;
@@ -39,6 +40,13 @@ function App() {
             return {locationName,timezone,description,icon,temperature,humidity,pressure,windSpeed};
 
         }
+
+        function isLocationNameAlreadyDisplayed(locations:LocationData[], locationName:string):boolean {
+            //checks if in locations array is any location with name equal to locationName.
+            //returns true if there is a location with name locationName and false otherwise.
+            return locations.find(location =>location.locationName === locationName ) !== undefined;
+        }
+
         if(lastIntroducedLocationName !== ""){
             const url=`https://api.openweathermap.org/data/2.5/weather?q=${lastIntroducedLocationName}&units=metric&appid=ef875aab8139ba42eefcd69852c22707`;
             fetch(url)
@@ -48,22 +56,39 @@ function App() {
                     }
                     return response.json();
                 })
-                .then(data=>{console.log(data);setFetchingError(null);setLocationData(extractWeatherData(data))})
-                .catch(error =>{console.log(error);setFetchingError(error)})
+                .then(data=>{
+                    const extractedData=extractWeatherData(data);
+                    setFetchError(null);
+                    setLocationsData(prevState =>{
+                        if(isLocationNameAlreadyDisplayed(prevState,extractedData.locationName)){
+                            return [...prevState];
+                        }
+                        return [...prevState,extractedData];
+                    })
+                })
+                .catch(error => setFetchError(error))
         }
     },[lastIntroducedLocationName]);
+
+
+    //map over locations
+    const weatherCards = locationsData.map(locationData => <WeatherCard locationName={locationData.locationName}
+                                                                        icon={locationData.icon}
+                                                                        pressure={locationData.pressure}
+                                                                        timezone={locationData.timezone}
+                                                                        humidity={locationData.humidity}
+                                                                        windSpeed={locationData.windSpeed}
+                                                                        description={locationData.description}
+                                                                        temperature={locationData.temperature}/>);
+
+
     return(
         <div className={"main-container"}>
-            {fetchingError && <FetchError message={fetchingError.message}/>}
+            {fetchError && <FetchError message={fetchError.message}/>}
             <LocationInput searchButtonClickHandler={searchButtonClickHandler}/>
-            {locationData.locationName !== "" && <WeatherCard locationName={locationData.locationName}
-            icon={locationData.icon}
-            pressure={locationData.pressure}
-            timezone={locationData.timezone}
-            humidity={locationData.humidity}
-            windSpeed={locationData.windSpeed}
-            description={locationData.description}
-            temperature={locationData.temperature}/>}
+            <div className={"weather-cards-container"}>
+                {locationsData.length>0 && weatherCards}
+            </div>
         </div>
     )
 }
